12efbfdb78e076633d488a5a4d0a33e5ac5042f9
meerhamzadev <hamzababar37@gmail.com>
2023-02-04 21:49:12 +0500

    👌 IMPROVED: Docker related files

--- swagger.js
+++ swagger.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')()
-
-const outputFile = './swagger/swagger_output.json'
+const path = require('path');
+const outputFile = path.join(__dirname, 'swagger', 'swagger_output.json');
 const endpointsFiles = ['./routes/indexRoutes.js']
 
 swaggerAutogen(outputFile, endpointsFiles)
